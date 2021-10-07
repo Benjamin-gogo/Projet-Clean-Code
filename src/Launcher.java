4645c0d213190b16eefcfcdef91db262e39464b4
anthony <jauch-anthony@hotmail.fr>
2021-10-07 18:57:21 +0200

    userstory1 wop

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,10 +1,33 @@
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Launcher {
-    public static void main(String[] args) {
-        System.out.println("Test 1");
-        System.out.println("efgetryhg Test 1");
+    public static void main(String[] args) throws IOException {
+        String one = " |\n|";
+        MyFileReader myFileReader = new MyFileReader();
+        StringBuilder builder = myFileReader.readMyFile("texte.txt");
+        for (int i = 0; i < builder.length(); i++) {
+            if (builder.charAt(i) == '_' && i < 23) {
+                System.out.print("2-3-5-6-7-8-9 ?");
+            } else {
+                if (builder.charAt(i) == '|' && i < 51 && i > 23 && builder.charAt(i + 1) == ' ' && builder.charAt(i - 1) != '_') {
+                    System.out.print("1-7 L2?");
+                    if (builder.charAt(i) == '|' && i < 80 && i > 51 && builder.charAt(i + 1) != ' ' && builder.charAt(i - 1) != '_') {
+                        System.out.print("1-7 L3?");
+                    }
+                }
+          /*  if (builder.charAt(i) == '|' && i < 51 && i > 23 && builder.charAt(i+1) == ' ' && builder.charAt(i-1) != '_'){
+                System.out.print("1-7?");
+            }*/
 
-        System.out.println("Test 22");
-        System.out.println("Test 23");
+      /*      if (builder.charAt(i) == '|' && i < 80 && i > 51 && builder.charAt(i+1) != ' ' && builder.charAt(i-1) != '_'){
+                System.out.print("1-7?");
+            }*/
+            }
+            System.out.println(builder.charAt(i) + " " + i);
+        }
     }
-
 }
+
+
