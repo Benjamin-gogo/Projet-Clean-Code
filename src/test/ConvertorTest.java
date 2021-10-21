2445e06bada10dae666f8ff3e7db38fdef48add4
anthony <jauch-anthony@hotmail.fr>
2021-10-21 22:50:47 +0200

    add convert test

---
+++ src/test/ConvertorTest.java
@@ -0,0 +1,23 @@
+package test;
+
+import main.java.Convertor;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConvertorTest {
+
+    @Test
+    void arrayEntryToString() {
+        ArrayList<Integer> intList = new ArrayList<Integer>(Arrays.asList(1,2,3,4,5,6,7,8,9));
+        assertEquals("123456789", Convertor.ArrayEntryToString(intList));
+    }
+
+    @Test
+    void checksumValueToString() {
+        assertEquals(" ", Convertor.ChecksumValueToString(0));
+    }
+}
